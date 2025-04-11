a21bf5b22cb45acb3dfd343a298880dd6dcfbe70
lontten <lontten@163.com>
2025-04-11 19:01:07 +0800

    ✨ 添加一个系统消息的示例项目

--- examples/hello/main.cpp
+++ examples/hello/main.cpp
@@ -5,8 +5,8 @@
 #include <clc/message.h>
 
 int main() {
-    // clc::win("一个clc 空白窗口");
-    auto ret = clc::message("标题", "内容", clc::MessageType::OkCancel, 0, clc::LangType::ENGLISH_US);
+    clc::win("一个clc 空白窗口");
+    auto ret = message("标题", "内容", clc::MessageType::OkCancel, 0, clc::LangType::ENGLISH_US);
     printf("ret = %d\n", ret);
     return 0;
 }
---
+++ examples/sys_info/main.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Lontten on 2025/4/2.
+//
+#include <clc/win.h>
+#include <clc/message.h>
+
+int main() {
+    clc::win("系统信息");
+    return 0;
+}
